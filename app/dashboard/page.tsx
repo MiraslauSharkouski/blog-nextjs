35f8c65f5ce944314a4508100111a0aa888674f8
Miraslau <miraslausharkouski@gmail.com>
2025-03-16 11:27:44 +0300

    add Create Post

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,16 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default async function DashboardRoute() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-xl font-medium">your blog articles</h2>
 
-  if (!user) {
-    return redirect("api/auth/register");
-  }
-
-  return <div>DashboardRoute</div>;
+        <Link className={buttonVariants()} href="/dashboard/create/">
+          Create Post
+        </Link>
+      </div>
+    </div>
+  );
 }
